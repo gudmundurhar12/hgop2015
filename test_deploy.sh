bb7cd50064afa424f704f32e5059aa6127431b02
gudmundurhar12 <gudmundurhar12@ru.is>
2015-11-30 11:17:17 +0000

    Fixing test_deploy script so it reads the hostname from a argument passed to the script

--- test_deploy.sh
+++ test_deploy.sh
@@ -1,3 +1,4 @@
 sudo service docker start
+echo ---- Pushing to Docker ----
 docker push hardag/tictactoe
-ssh root@162.243.121.139 "sudo pkill -9 docker; sudo service docker start && docker pull hardag/tictactoe && docker run -p 80:8080 -d -e "NODE_ENV=production" hardag/tictactoe"
+ssh root@$1 "sudo pkill -9 docker; docker rm -f $(docker ps -a -q); docker rmi -f $(docker images -q); sudo service docker start && docker pull hardag/tictactoe && docker run -p 80:8080 -d -e "NODE_ENV=production" hardag/tictactoe && echo ---- Docker running on $1 ----"
