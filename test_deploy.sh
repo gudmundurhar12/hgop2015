6089681a21dd6e226e11005d8272a6d015a487c0
gudmundurhar12 <gudmundurhar12@ru.is>
2015-11-30 09:44:21 +0000

    fixing and optimizing test_deploy script

--- test_deploy.sh
+++ test_deploy.sh
@@ -1,2 +1,3 @@
+sudo service docker start
 docker push hardag/tictactoe
-ssh 192.168.33.10 docker run -p 100:8080 -d -e "NODE_ENV=production" hardag/tictactoe
+ssh root@162.243.121.139 "sudo pkill -9 docker; sudo service docker start && docker pull hardag/tictactoe && docker run -p 80:8080 -d -e "NODE_ENV=production" hardag/tictactoe"
