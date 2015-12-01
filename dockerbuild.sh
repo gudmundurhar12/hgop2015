0191112efec77903044c9b53be2832813d33b5bd
gudmundurhar12 <gudmundurhar12@ru.is>
2015-12-01 17:37:52 +0000

    Tweeking changes to dockerbuild script

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -9,20 +9,20 @@ sudo npm install
 [ $? -eq 0 ] || exit $?
 
 echo Bower Install
-sudo bower install
+bower install
 [ $? -eq 0 ] || exit $?
 
 echo Cleaning...
 rm -rf ./dist
 
 echo Building app
-sudo grunt
+grunt
 [ $? -eq 0 ] || exit $?
 
 cp ./Dockerfile ./dist/
 
 cd dist
-sudo npm install --production
+npm install --production
 
 echo Building docker image
 docker build -t hardag/tictactoe . && echo "Done"
