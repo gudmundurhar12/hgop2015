dad38e873dbde077851bf53a6495e87693690d70
gudmundurhar12 <gudmundurhar12@ru.is>
2015-12-02 19:25:26 +0000

    Added karma-junit-reporter to the project

--- karma.conf.js
+++ karma.conf.js
@@ -71,7 +71,15 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     browsers: ['PhantomJS'],
+    
+    reporters: ['progress', 'junit'],
 
+    junitReporter: {
+      outputDir: 'test_reports', // results will be saved as $outputDir/$browserName.xml
+      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
+      suite: '', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: true // add browser name to report and classes names
+    },
 
     // Continuous Integration mode
     // if true, it capture browsers, run tests and exit
