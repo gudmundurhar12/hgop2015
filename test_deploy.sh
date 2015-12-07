da52d7853cb7c2732be1d69e7352603c80a3a5ab
gudmundurhar12 <gudmundurhar12@ru.is>
2015-12-07 17:54:03 +0000

    Fixing test_deploy script

--- test_deploy.sh
+++ test_deploy.sh
@@ -4,8 +4,10 @@ rc=$?; if [[ $rc != 0 ]]; then echo "Docker didn't start"; exit $rc; fi
 echo ---- Pushing to Docker ----
 yes | docker push hardag/tictactoe
 
-ssh root@$1 "sudo pkill -9 docker; docker rm -f $(docker ps -a -q); docker rmi -f $(docker images -q); sudo service docker start && docker pull hardag/tictactoe && docker run -p 80:8080 -d -e "NODE_ENV=production" hardag/tictactoe && echo ---- Docker running on $1 ---- && "
+echo ---- Starting docker on $1 ----
+ssh root@$1 "sudo pkill -9 docker; docker rm -f $(docker ps -a -q); docker rmi -f $(docker images -q); sudo service docker start && docker pull hardag/tictactoe && docker run -p 80:8080 -d -e "NODE_ENV=production" hardag/tictactoe && echo ---- Docker running on $1 ----"
 
-export ACCEPTANCE_URL=http://162.243.121.139
+echo ---- run Acceptance Test on $1 ----
+export ACCEPTANCE_URL=http://$1
 grunt mochaTest:acceptance
 rc=$?; if [[ $rc != 0 ]]; then echo "Acceptance test failed"; exit $rc; fi
