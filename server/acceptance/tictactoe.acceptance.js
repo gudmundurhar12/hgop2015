c03ab99cbaf9441489dd930449a58b542f97c230
gudmundurhar12 <gudmundurhar12@ru.is>
2015-12-08 23:54:51 +0000

    implemented functions for fluid API test requirements

--- server/acceptance/tictactoe.acceptance.js
+++ server/acceptance/tictactoe.acceptance.js
@@ -4,6 +4,60 @@ var should = require('should');
 var request = require('supertest');
 var acceptanceUrl = process.env.ACCEPTANCE_URL;
 
+function user(name){
+
+  const cmd = {
+    id : "1",
+    gameId : "999",
+    command: undefined,
+    userName: name,
+    name: undefined,
+    timeStamp: "2015-12-07T11:29:29"
+  };
+
+  var apiCommand = {
+    createsGame: function(gameName){
+      cmd.command = "CreateGame";
+      cmd.name = gameName;
+
+      return cmd;
+    }
+  };
+  return apiCommand;
+}
+
+
+function given(cmd){
+
+  var expectations = [];
+  var expectation = {};
+  var givenApi = {
+    expect: function(eventType){
+      expectation.event = eventType;
+      return givenApi;
+    },
+    withName: function(gameName){
+      expectation.gameName = gameName;
+      return givenApi;
+    },
+    isOk: function(done){
+      const req = request(acceptanceUrl);
+      req
+      .post('/api/createGame')
+      .type('json')
+      .send(cmd)
+      .end((err, res) => {  
+        if (err) return done(err);
+          should(res.body[res.body.length - 1]).have.property('event', expectation.event);
+          should(res.body[res.body.length - 1]).have.property('name', expectation.gameName);
+          done();
+      });
+    }
+  };
+  return givenApi;
+}
+
+
 
 describe('TEST ENV GET /api/gameHistory', function () {
 
@@ -15,7 +69,7 @@ describe('TEST ENV GET /api/gameHistory', function () {
 
     var command =     {
       id : "1",
-      gameId : "999",
+      gameId : "888",
       command: "CreateGame",
       userName: "Gummi",
       name: "FirstGame",
@@ -30,7 +84,7 @@ describe('TEST ENV GET /api/gameHistory', function () {
       .end(function (err, res) {
         if (err) return done(err);
         request(acceptanceUrl)
-          .get('/api/gameHistory/999')
+          .get('/api/gameHistory/888')
           .expect(200)
           .expect('Content-Type', /json/)
           .end(function (err, res) {
@@ -39,7 +93,7 @@ describe('TEST ENV GET /api/gameHistory', function () {
             should(res.body).eql(
               [{
                 "id": "1",
-                "gameId": "999",
+                "gameId": "888",
                 "event": "GameCreated",
                 "userName": "Gummi",
                 "name": "FirstGame",
@@ -52,11 +106,9 @@ describe('TEST ENV GET /api/gameHistory', function () {
 
 
    it('Should execute fluid API test', function (done) {
-     /*
+     
      given(user("Gummi").createsGame("TheFirstGame"))
      .expect("GameCreated").withName("TheFirstGame").isOk(done);
-      */
-     done();
    });
 
 });
