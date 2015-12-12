2776297ec1dbd17658471c4ef47ce6ed68ef340b
gudmundurhar12 <gudmundurhar12@ru.is>
2015-12-12 11:39:16 +0000

    Adding changes to test_deploy and dockerbuild scripts

--- test_deploy.sh
+++ test_deploy.sh
@@ -1,9 +1,6 @@
 sudo service docker start
 rc=$?; if [[ $rc != 0 ]]; then echo "Docker didn't start"; exit $rc; fi
 
-echo ---- Pushing to Docker ----
-yes | docker push hardag/tictactoe
-
 echo ---- Starting docker on $1 ----
 ssh root@$1 "sudo service docker stop ; sudo pkill -9 docker; docker rm -f $(docker ps -aq); docker rmi -f $(docker images -q); sudo service docker start ; docker pull hardag/tictactoe && docker run -p 80:8080 -d -e "NODE_ENV=production" hardag/tictactoe && echo ---- Docker running on $1 ----"
 
